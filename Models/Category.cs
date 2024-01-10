a7cf351a3064e90180f88aecbb56d59ec8fc359c
Mohammed Ragab El Ashmoney <98624415+El-Ashmoney@users.noreply.github.com>
2024-01-10 16:18:28 +0200

    fixing the Nullable error in Category Model

--- Models/Category.cs
+++ Models/Category.cs
@@ -5,7 +5,7 @@ public class Category{
     [Key]
     public int Id { get; set; }
     [Required]
-    public string Name { get; set; }
+    public string? Name { get; set; }
     public int DisplayOrder { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.Now;
 }
